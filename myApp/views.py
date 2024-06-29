3c7ddab2c23adedf1a5e73a2f68bb83102d8345f
SnayderCJ <jcedenoj5@unemi.edu.ec>
2024-06-29 18:04:38 -0500

    'Update'

--- myApp/views.py
+++ myApp/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
-def hello(request):
-    return HttpResponse('Hello World')
+def index(request):
+    return HttpResponse('Index Page')
+
+def hello(request, username):
+    return HttpResponse('<h2> Hello World %s </h2>' % username)
 
 def about(request):
     return HttpResponse('About')
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, title):
+    task = Task.objects.get(title=title)
+    return HttpResponse('task: %s' % task.title)
